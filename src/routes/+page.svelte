a0ddeecaf1cc67749cba3a5cb155d5dff0d52bf5
mjamesderocher <mjamesderocher@gmail.com>
2023-08-27 15:27:11 +0300

    Added DXOS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Client } from "@dxos/client";
+  import { Expando } from "@dxos/client/echo";
+
+  // create a client
+  const client = new Client();
+
+  let tasks = [];
+  let space;
+  let identity;
+  let text = "";
+
+  const main = async () => {
+    await client.initialize();
+
+    identity = client.halo.identity.get();
+
+    const spaces = client.spaces.get();
+
+    if (spaces.length > 0) {
+      const key = spaces[0].key;
+
+      space = client.getSpace(key);
+    } else {
+      space = await client.createSpace({ name: "todos" });
+    }
+  };
+
+  main();
+
+  $: if (space) {
+    space.db.query({ type: "task" }).subscribe((a) => {
+      tasks = a;
+    });
+  }
+
+  const handleSubmit = () => {
+    // Create object
+    const object = new Expando({ type: "task", title: text });
+
+    // Adds object
+    space.db.add(object);
+
+    // Resets text input
+    text = "";
+  };
+</script>
+
+<h1>Todos</h1>
+
+<form on:submit|preventDefault={() => handleSubmit()}>
+  <input bind:value={text} />
+  <input type="submit" value="add task" />
+</form>
+
+{#if tasks.objects}
+  <ul>
+    {#each tasks.objects as task}
+      <li>Title: {task.title}</li>
+    {/each}
+  </ul>
+{/if}
